4a38b18c6c60d9c052b2b356f2ca55a41217227f
Predrag Cujanovic <predrag@cujanovic.com>
2020-09-11 18:54:58 +0200

    update

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ Payloads from BB reports for Open Redirect
 
 ***
 
-### Replace www.whitelisteddomain.tld with a specific whitelisted domain in your test case
+### make-payloads.sh - replace www.whitelisteddomain.tld with a specific whitelisted domain in your test case
 
-To do this simply modify the WHITELISTEDDOMAIN with value www.test.com to your test case URL.
+./make-payloads.sh www.whitelisteddomain.pw
 
-`WHITELISTEDDOMAIN="www.test.com" && sed 's/www.whitelisteddomain.tld/'"$WHITELISTEDDOMAIN"'/' Open-Redirect-payloads.txt > Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp && sed 's/@www.whitelisteddomain.tld/@'"$WHITELISTEDDOMAIN"'/' Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp > Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt && echo "$WHITELISTEDDOMAIN" | awk -F. '{if ($1 =="www") print "//not"$2"."$NF"/"; else print "//not"$1"."$NF"/";}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt && echo "$WHITELISTEDDOMAIN" | awk -F. '{if ($1 =="www") print "http://not"$2"."$NF"/"; else print "http://not"$1"."$NF"/";}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt && echo "$WHITELISTEDDOMAIN" | awk -F. '{print "http://"$0"."$NF"/"}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt && rm -f Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp && echo -e "\nDone. Filename: $(pwd)/Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt"`
+./make-payloads.sh www.google.com
+
+./make-payloads.sh app.domain.com
 
 ***
---
+++ make-payloads.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#If terminal add collors
+if [[ -t 1 ]]; then
+	#Colors
+	red='\e[0;31m'
+	green='\e[0;32m'
+	blue='\e[0;36m'
+	yellow='\e[0;33m'
+	errorredline='\e[0;41m'
+	greenbgblacktext='\e[30;48;5;82m'
+	NC='\e[0m'
+else
+	#NoColors
+	red=''
+	green=''
+	blue=''
+	yellow=''
+	errorredline=''
+	greenbgblacktext=''
+	NC=''
+fi
+
+WHITELISTEDDOMAIN="$1"
+
+function PRINT_CORRECT_USAGE_EXAMPLE () {
+	echo -e "$0 www.whitelisteddomain.pw\n"
+	echo -e "${green}$0 www.google.com\n${NC}"
+	echo -e "${blue}$0 app.domain.com\n${NC}"
+}
+
+function CHECK_PREVIOUS_CMD () {
+if [ $? -ne 0 ]; then
+	echo -e "${errorredline}Build error${NC}"
+	echo -e "${red}$0 script error at line: $1 ${NC}"
+	echo -e "${yellow}View the last log above.${NC}"
+	exit 1
+fi
+}
+
+function MAKE_REDIRECT_PAYLOADS () {
+	sed 's/www.whitelisteddomain.tld/'"$WHITELISTEDDOMAIN"'/' Open-Redirect-payloads.txt > Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp
+	CHECK_PREVIOUS_CMD $LINENO
+	sed 's/@www.whitelisteddomain.tld/@'"$WHITELISTEDDOMAIN"'/' Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp > Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt
+	CHECK_PREVIOUS_CMD $LINENO
+	echo "$WHITELISTEDDOMAIN" | awk -F. '{if ($1 =="www") print "//not"$2"."$NF"/"; else print "//not"$1"."$NF"/";}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt
+	CHECK_PREVIOUS_CMD $LINENO
+	echo "$WHITELISTEDDOMAIN" | awk -F. '{if ($1 =="www") print "http://not"$2"."$NF"/"; else print "http://not"$1"."$NF"/";}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt
+	CHECK_PREVIOUS_CMD $LINENO
+	echo "$WHITELISTEDDOMAIN" | awk -F. '{print "http://"$0"."$NF"/"}' >> Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".txt
+	rm -f Open-Redirect-payloads-burp-"$WHITELISTEDDOMAIN".tmp
+	echo -e "\n${greenbgblacktext}Done.${NC} Filename: $(pwd)/Open-Redirect-payloads-burp-$WHITELISTEDDOMAIN.txt"
+}
+
+if [ $# -eq 0 ] || [ $# -gt 1 ] || [ $# -lt 1 ]; then
+	echo -e "${NC}"
+	echo -e "${red}Invalid number of arguments. Usage:\n${NC}"
+	PRINT_CORRECT_USAGE_EXAMPLE
+	exit 1
+else
+	MAKE_REDIRECT_PAYLOADS
+fi
